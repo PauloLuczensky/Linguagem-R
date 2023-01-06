4dea31515abd76e08bc0bf494245e251b728b72c
Paulo Luczensky <99833304+PauloLuczensky@users.noreply.github.com>
2023-01-06 12:32:29 -0300

    Add files via upload

---
+++ Variaveis.R
@@ -0,0 +1,40 @@
+#Início
+
+#Declarando variáveis
+#Armazenando valores nas variáveis x e y
+x <- 22
+y <- 23
+
+#Armazenando a soma na variável z
+x + y -> z
+#Printando valor de z
+z
+
+x - y -> b
+
+b
+
+#--------------------
+
+#Soma de um INTEGER  e um NUMERIC
+ 
+p <- 10L
+
+s <- 12.2
+
+p + s -> w
+
+w
+
+#Numero complexo
+a <- 4i
+
+#String
+o <- "Hello"
+
+#Boolean
+
+u <- TRUE
+
+
+
