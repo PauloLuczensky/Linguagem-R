e72bb0af8bec81f74f5c71cb147ef3a0049e1e95
Paulo Luczensky <99833304+PauloLuczensky@users.noreply.github.com>
2023-01-06 12:19:53 -0300

    Add files via upload

---
+++ VECTORS-R.R
@@ -0,0 +1,27 @@
+#Vectors
+
+#THERE ARE 5 ATOMIC VECTORS, ALSO TERMED AS FIVE LASSES OF VECTORS
+#1- LOGICAL
+#2- INTEGER
+#3- NUMERIC
+#4- COMPLEX
+#5- CHARACTER
+
+#Logical
+vtr1 = c(TRUE, FALSE)
+
+#Numeric
+vtr2 = c(15, 85.6747474, 99999)
+
+#Integer
+#Para considerar um inteiro, tem que colocar a letra L
+vtr3 = c(35L, 58L, 34L)
+
+#OBSERVAÇÕES
+vtr4 = c(58.465L) # dará erro
+
+vtr5 = c(TRUE, 35L, 3.14) #quando colocar vtr5 no console, 
+#mostrará o valor 1 no True
+
+vtr6 = c("Hello", FALSE, 65L)# quando colocar o vtr6 no console para print,
+#todos serão transformados em caracteres
